3a8eaf8ea5eeb6147d1b792285adcf0e7b7212d6
controny <0755dimitri@gmail.com>
2019-11-16 17:16:37 +0800

    beautify deployment output

--- start_all.py
+++ start_all.py
@@ -9,9 +9,9 @@ print('command to start name node: ', command_start)
 os.system(command_start)
 
 for slave in host_list:
-    print('send DFS codes to slave [%s]' % slave)
-    command_send_codes = 'ssh %s "mkdir -p %s"; scp *.py %s:%s' % (slave, cur_dir, slave, cur_dir)
+    print('sending DFS codes to slave [%s]' % slave)
+    command_send_codes = 'ssh %s "mkdir -p %s"; scp *.py %s:%s > /dev/null' % (slave, cur_dir, slave, cur_dir)
     os.system(command_send_codes)
     command_start = 'ssh %s "python3 %s/data_node.py " &' % (slave, cur_dir)
-    print('command to start data node [%s]: %s' % (slave, command_start))
+    print('trying to start data node [%s]' % slave)
     os.system(command_start)
--- stop_all.py
+++ stop_all.py
@@ -7,12 +7,12 @@ command_kill_pattern = 'kill $(ps -ef | grep "python3 %s" | grep -v grep | tr -s
 
 # stop name node
 command_kill = command_kill_pattern % os.path.join(cur_dir, 'name_node.py')
-print('command to kill name node: ', command_kill)
+print('trying to kill name node')
 os.system(command_kill)
 
 # stop all data nodes
 command_kill = command_kill_pattern % os.path.join(cur_dir, 'data_node.py')
 for slave in host_list:
     command_remote_kill = 'ssh %s \'%s\'' % (slave, command_kill)
-    print('command to kill data node [%s]: %s' % (slave, command_remote_kill))
+    print('trying to kill data node [%s]' % slave)
     os.system(command_remote_kill)
