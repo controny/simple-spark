efa33a4e3e4508d084bc524214572178d885e064
controny <0755dimitri@gmail.com>
2019-11-17 19:03:49 +0800

    rewrite command to kill

--- stop_all.py
+++ stop_all.py
@@ -2,17 +2,15 @@ from common import *
 import os
 
 cur_dir = os.getcwd()
-# TODO kill process by port using `lsof -i:[port]`
-# Use `tr -s " "` to squeezes the spaces of ps output together, refer to https://stackoverflow.com/a/15643939
-command_kill_pattern = 'kill $(ps -ef | grep "python3 %s" | grep -v grep | tr -s " " | cut -d " " -f 2)'
+command_kill_pattern = 'kill $(lsof -t -i:%d)'
 
 # stop name node
-command_kill = command_kill_pattern % os.path.join(cur_dir, 'name_node.py')
+command_kill = command_kill_pattern % name_node_port
 print('trying to kill name node')
 os.system(command_kill)
 
 # stop all data nodes
-command_kill = command_kill_pattern % os.path.join(cur_dir, 'data_node.py')
+command_kill = command_kill_pattern % data_node_port
 for slave in host_list:
     command_remote_kill = 'ssh %s \'%s\'' % (slave, command_kill)
     print('trying to kill data node [%s]' % slave)
