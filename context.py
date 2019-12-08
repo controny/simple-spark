5433a2e056e8c9dd86d0f2e5fca5882b297dad8f
oym <oym@oyms-macbook-air.local>
2019-12-08 23:12:10 +0800

    little diff in context,py

--- context.py
+++ context.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from rdd import RDD
+from uniqueID import *
 
 class SparkContext(RDD):
-    pass
+    # confusing with def
+    def read(self,file_adress):
+       pass
