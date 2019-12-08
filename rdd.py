08afffdfc94d00287677fb509a32d35ef2a77a91
oym <oym@oyms-macbook-air.local>
2019-12-08 22:39:42 +0800

    uniqueID and prototype of RDD

--- rdd.py
+++ rdd.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from uniqueID import *
 
 class RDD:
-    pass
+# To do list:
+# method
+# point to parent and child 
+# store operation
+# unique ID
+    def __init__(self,child,parent=None):
+        self.parent = parent 
+        self.child = child
+        self.ID = next(unique_sequence)
+    def flatMap(self):
+        self.operation = "flatMap" 
+    def Map(self):
+        self.operation = "Map" 
+    def cache(self):
+        self.operation = "cache"
+    def textFile(self):
+        self.operation = "textFile"
+    
