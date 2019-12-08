a5dd7228adffc14421c3b9dcfe9266c32da68efd
oym <oym@oyms-macbook-air.local>
2019-12-08 23:33:03 +0800

    prototype of RDD

--- rdd.py
+++ rdd.py
@@ -4,26 +4,32 @@ from uniqueID import *
 
 class RDD:
 # To do list:
-# method
-# point to parent and child # seems to be done 
-# store operation #
-# unique ID
+# method not ,sure
+# point to parent and child ,Done 
+# store operation ,Done 
+# unique ID  ,Done
+# ---------Discussion-----------
+# should we store partitions?
     def __init__(self,parent=None,operation=None):
         self.parent = parent
         self.operation = operation 
         self.ID = next(unique_sequence)
+
     def flatMap(self):
         child = RDD(parent=self,operation="flatMap")
         self.child = child
         return child
+    
     def Map(self):
         child = RDD(parent=self,operation="Map")
         self.child = child
         return child
+    
     def cache(self):
         child = RDD(parent=self,operation="cache")
         self.child = child
         return child
+    
     def textFile(self):
         child = RDD(parent=self,operation="textFile")
         self.child = child
